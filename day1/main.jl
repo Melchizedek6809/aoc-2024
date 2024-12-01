724073d99d8790b14024b01dd9cc599ccda7adca
Ben (MBA M2) <ben@wolkenwelten.net>
2024-12-01 23:32:59 +0200

    Tried cleaning up day1

--- day1/main.jl
+++ day1/main.jl
@@ -1,20 +1,31 @@
-function read_file(filename)
+"""
+Reads a file and returns its contents as a string.
+"""
+function read_file(filename::String)::String
 	f = open(filename, "r")
 	s = read(f, String)
 	close(f)
 	return s
 end
 
-function parse_line(line)
+
+"""
+Parses a line into two integers.
+"""
+function parse_line(line::SubString{String})
 	cols = split(line, "   ")
-	left = parse(Int, cols[1])
-	right = parse(Int, cols[2])
+	left = parse(Int64, cols[1])
+	right = parse(Int64, cols[2])
 	return (left, right)
 end
 
-function parse_lines(lines)
-	leftList = []
-	rightList = []
+
+"""
+Parses a list of lines into two sorted lists of integers.
+"""
+function parse_lines(lines::Vector{SubString{String}})
+	leftList::Vector{Int64} = []
+	rightList::Vector{Int64} = []
 	for line in lines
 		left, right = parse_line(line)
 		push!(leftList, left)
@@ -25,8 +36,12 @@ function parse_lines(lines)
 	return (leftList, rightList)
 end
 
-function calculate_total(leftList, rightList)
-	total = 0
+
+"""
+Calculates the total distance between two lists of integers.
+"""
+function calculate_total(leftList::Vector{Int64}, rightList::Vector{Int64})::Int64
+	total::Int64 = 0
 	for i in eachindex(leftList, rightList)
 		dist = abs(leftList[i] - rightList[i])
 		total += dist
@@ -34,15 +49,23 @@ function calculate_total(leftList, rightList)
 	return total
 end
 
-function part_one()
-	lines = split(read_file("data.txt"), "\n")
-	leftList, rightList = parse_lines(lines)
+
+"""
+Calculates the total distance between two lists of integers and prints it.
+"""
+function part_one(leftList::Vector{Int64}, rightList::Vector{Int64})
 	total = calculate_total(leftList, rightList)
 	println(total)
 end
 
-function turn_list_to_map(list)
-	map = Dict()
+
+
+
+"""
+Turns a list of integers into a map of integers to their counts.
+"""
+function turn_list_to_map(list::Vector{Int64})::Dict{Int64, Int64}
+	map::Dict{Int64, Int64} = Dict()
 	for i in eachindex(list)
 		if !haskey(map, list[i])
 			map[list[i]] = 0
@@ -52,8 +75,12 @@ function turn_list_to_map(list)
 	return map
 end
 
-function calculate_similarity(leftList, rightMap)
-	similarity = 0
+
+"""
+Calculates the similarity between a list of integers and a map of integers to their counts.
+"""
+function calculate_similarity(leftList::Vector{Int64}, rightMap::Dict{Int64, Int64})::Int64
+	similarity::Int64 = 0
 	for i in eachindex(leftList)
 		if haskey(rightMap, leftList[i])
 			similarity += leftList[i] * rightMap[leftList[i]]
@@ -62,13 +89,18 @@ function calculate_similarity(leftList, rightMap)
 	return similarity
 end
 
-function part_two()
-	lines = split(read_file("data.txt"), "\n")
-	leftList, rightList = parse_lines(lines)
+
+"""
+Calculates the similarity between a list of integers and a map of integers to their counts and prints it.
+"""
+function part_two(leftList::Vector{Int64}, rightList::Vector{Int64})
 	rightMap = turn_list_to_map(rightList)
 	similarity = calculate_similarity(leftList, rightMap)
 	println(similarity)
 end
 
-part_one()
-part_two()
+
+lines = split(read_file("data.txt"), "\n")
+leftList, rightList = parse_lines(lines)
+part_one(leftList, rightList)
+part_two(leftList, rightList)
