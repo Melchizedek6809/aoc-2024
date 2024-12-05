ee752bea3a0c02e04fa14f9fc1d5db22d5909be7
Ben (MBA M2) <ben@wolkenwelten.net>
2024-12-05 20:57:57 +0200

    5.2

--- day5/main.jl
+++ day5/main.jl
@@ -56,6 +56,55 @@ function parse_order(rules::Dict{Int,Set{Int}}, order::SubString)::Vector{Vector
 	return filtered_order
 end
 
+function ensure_order(rules::Dict{Int,Set{Int}}, values::Vector{Int})::Vector{Int}
+	sorted_values = []
+
+	all_vals = Set{Int}()
+	for value in values
+		push!(all_vals, value)
+	end
+
+	vals = Set{Int}()
+	for value in values
+		if value in vals
+			continue
+		end
+
+		if haskey(rules, value)
+			current_rules = rules[value]
+			for rule in current_rules
+				if rule in all_vals && !(rule in vals)
+					push!(sorted_values, rule)
+					push!(vals, rule)
+				end
+			end
+		end
+	
+		push!(vals, value)
+		push!(sorted_values, value)
+	end
+	return sorted_values
+end
+
+function parse_order_or_sort(rules::Dict{Int,Set{Int}}, order::SubString)::Vector{Vector{Int}}
+	order = split(order, "\n")
+	filtered_order = []
+	for line in order
+		values = parse.(Int, split(line, ","))
+		if is_valid(rules, values)
+			continue
+		end
+
+		while !is_valid(rules, values)
+			values = ensure_order(rules, values)
+		end
+
+		push!(filtered_order, values)
+	end
+	return filtered_order
+end
+
+
 function get_mid_values(filtered_order::Vector{Vector{Int}})::Vector{Int}
 	mid_values = []
 	for values in filtered_order
@@ -75,3 +124,15 @@ function part_one()
 end	
 
 part_one()
+
+function part_two()
+	source = read_file("input.txt")
+	rules, order = split(source, "\n\n")
+	rules = parse_rules(rules)
+	filtered_order = parse_order_or_sort(rules, order)
+	mid_values = get_mid_values(filtered_order)
+	sum = reduce(+, mid_values)
+	println(sum)
+end	
+
+part_two()
